b76febccd37e5d44c90aa56bf025dcad0b1187a8
nishantravi92 <nishantravi92@gmail.com>
2016-10-08 22:52:05 -0400

    Create TimePicker App

---
+++ TimePicker/app/src/main/java/com/whiteyolk/nishantr/timepicker/MainActivity.java
@@ -0,0 +1,26 @@
+package com.whiteyolk.nishantr.timepicker;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+}
